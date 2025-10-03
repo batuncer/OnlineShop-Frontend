66f1ee539236830b390c18de235967eaf010e046
Tuncer <baki.tuncer@capgemini.com>
2025-10-03 15:52:48 +0100

    persist auth token and cart(basket) states with redux-persist

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,16 @@ import UserPage from "./pages/UserPage";
 
 // Routes
 import ProtectedRoute from "./routes/ProtectedRoute";
+import PublicRoute from "./routes/PublicRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Public Routes */}
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<PublicRoute><Navigate to="/login" /></PublicRoute>} />
+        <Route path="/login" element={<PublicRoute><LoginPage /></PublicRoute>} />
+        <Route path="/register" element={<PublicRoute><RegisterPage /></PublicRoute>} />
 
         {/* Private Route */}
         <Route path="/user/me" element={<ProtectedRoute><UserPage /></ProtectedRoute>} />
--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
   // Check for token in Redux state or localStorage
-  const token = useSelector((s)=>s.auth.token) || localStorage.getItem("token");
+  const token = useSelector((s)=>s.auth.token);
 
   // If no token, redirect to login
   if (!token) return <Navigate to="/login" replace />;
